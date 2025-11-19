9f376a7f59e92721d674153ee19ce5d404b1294d
Andry0304 <andreamagni03@gmail.com>
2025-11-19 11:03:46 +0100

    jar creation

--- AlgorithmRunner.java
+++ AlgorithmRunner.java
@@ -2,15 +2,17 @@ import java.awt.image.BufferedImage;
 import javax.swing.SwingUtilities;
 
 public class AlgorithmRunner implements Runnable {
+    
+    private static int width = 50;
+    private static int height = 30;
+    private static int shapeSize = 20;
+    private static Type shapeType = Type.SQUARE; // 4: square, 6: hexagon, 3: triangle, (add to enum new Type accordingly)
+    private static int time = 3;
 
     private LabyrinthAlgorithm labyrinthAlgorithm;
     private LabyrinthDrawer labyrinthDrawer;
     private Thread thread;
     private int dimension;
-    static int width = 50;
-    static int height = 30;
-    static int shapeSize = 20;
-    static Type shapeType = Type.SQUARE; // 4: square, 6: hexagon, 3: triangle, (add to enum new Type accordingly)
 
     public static void main(String[] args) {
         /*
@@ -25,24 +27,64 @@ public class AlgorithmRunner implements Runnable {
             }
         */
         
-        if (args.length > 0) {
+        for (int i = 0; i < args.length; i += 2) {
             try {
-                width = Integer.parseInt(args[0]);
-                height = Integer.parseInt(args[1]);
-                shapeSize = Integer.parseInt(args[2]);
-                shapeType = switch (args[3].toLowerCase()) {
-                    case "square" -> Type.SQUARE;
-                    case "hexagon" -> Type.HEXAGON;
-                    case "triangle" -> Type.TRIANGLE;
-                    default -> Type.SQUARE;
-                };
-            } catch (NumberFormatException e) {
-                System.out.println("Using default width value: " + width);
-                System.out.println("Using default height value: " + height);
-                System.out.println("Using default shape size value: " + shapeSize);
-                System.out.println("Using default shape type value: square");
+                String value = args[i + 1];
+                switch (args[i].toLowerCase()) {
+                    case "-width":
+                        try {
+                            width = Integer.parseInt(value);
+                        } catch (Exception e) {
+                            System.out.println("Error: value \"" + value + "\" not valid for propertie -width");
+                        }
+                        break;
+
+                    case "-height":
+                        try {
+                            height = Integer.parseInt(value);
+                        } catch (Exception e) {
+                            System.out.println("Error: value \"" + value + "\" not valid for propertie -height");
+                        }
+                        break;
+
+                    case "-shapesize":
+                        try {
+                            shapeSize = Integer.parseInt(value);
+                        } catch (Exception e) {
+                            System.out.println("Error: value \"" + value + "\" not valid for propertie -shapesize");
+                        }
+                        break;
+
+                    case "-shapetype":
+                        try {
+                            switch (value.toLowerCase()) {
+                                case "square" -> shapeType = Type.SQUARE;
+                                case "hexagon" -> shapeType = Type.HEXAGON;
+                                case "triangle" -> shapeType = Type.TRIANGLE;
+                                default -> new Exception();
+                            };
+                        } catch (Exception e) {
+                            System.out.println("Error: value \"" + value + "\" not valid for propertie -shapetype");
+                        }
+                        break;
+
+                    case "-time":
+                        try {
+                            time = Integer.parseInt(value);
+                        } catch (Exception e) {
+                            System.out.println("Error: value \"" + value + "\" not valid for propertie -time");
+                        }
+                        break;
+                
+                    default:
+                        System.out.println("Error: propertie \"" + args[i] + "\" not found");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Error: missing value");
             }
-        } 
+        }
+        
     
         SwingUtilities.invokeLater(() -> {
             AlgorithmRunner runner = new AlgorithmRunner(width, height, shapeSize, shapeType);
